369da4e6bc51f05efe67947cadca71bc12cf57af
Maksim Padhaiski <maksimpadhaiski@gmail.com>
2023-04-30 16:22:29 +0300

    RESTful Web Service

--- initial/src/main/java/com/example/restservice/GreetingController.java
+++ initial/src/main/java/com/example/restservice/GreetingController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
-	private static final String TEMPLATE = "Hello, %s!";
+	
+	private static final String TEMPLATE= "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
 	}
+
 }
